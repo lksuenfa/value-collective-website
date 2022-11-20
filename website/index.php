7894221fe7c9e4164f9310ec7d79a0d1e5ded35a
Elitrmb <117299877+Elitrmb@users.noreply.github.com>
2022-11-20 12:26:01 -0500

    adding some decorations and residencies image

--- website/index.php
+++ website/index.php
@@ -58,12 +58,32 @@ SOFTWARE.
 
 <div class="dropdown">
   <button id="myBtn" class="dropbtn"><p> residencies </p>
+
 </button>
   <div id="myDropdown" class="dropdown-content">
+
+    <img src="assets/images/residencies1.png" width="100%" alt="" img/>
+
 
 <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </div>
 </div>
+
+
+
+<div class="button2">
+  <button id="myBtn" class="dropbtn2"><p> projects </p>
+
+</button>
+  <div id="myButton2" class="button2-content">
+
+
+<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+</div>
+
+
+
 
 
 </div>
