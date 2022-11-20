110063b2e2b21a2198be7aec6c994c609e958139
Elitrmb <117299877+Elitrmb@users.noreply.github.com>
2022-11-20 11:46:58 -0500

    adding js for unrolling buttons

--- website/index.php
+++ website/index.php
@@ -56,8 +56,16 @@ SOFTWARE.
 </svg>
 </div>
 
-<p> residencies </p>
-<p> projects </p>
+<div class="dropdown">
+  <button id="myBtn" class="dropbtn"><p> residencies </p>
+</button>
+  <div id="myDropdown" class="dropdown-content">
+
+<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+</div>
 
 
 </div>
+
+<script src="js/frontpage.js"></script>
