31bc24cd24f775b9a113a2a40bce7b083d190b7e
Elitrmb <117299877+Elitrmb@users.noreply.github.com>
2022-11-21 13:42:45 -0500

    working on about page

--- website/about.php
+++ website/about.php
@@ -1,7 +1,23 @@
 <?php require "main/header.php" ?>
-<?php require "main/nav.php" ?>
 
-<div class="outer-box-frame">
-    <h1>About</h1>
+<section class="body">
+  <?php require "main/nav.php" ?>
+
+
+
+
+<section>
+
+  <div id="aboutpage" class="outer-box-frame">
+
+    <img src="assets/images/vc1_front.png" width="100%" alt="" />
+    <h1>About Projects at the VC</h1>
+
 </div>
+
+<link rel="stylesheet" href="css/about.css" />
+
+
+</section>
+</section>
 <?php require  "main/footer.php" ?>
