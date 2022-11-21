3cb583811683ec931dbdc2b7cd1828c100b915df
Elitrmb <117299877+Elitrmb@users.noreply.github.com>
2022-11-21 14:22:57 -0500

    working on timeline

--- website/about.php
+++ website/about.php
@@ -11,13 +11,97 @@
   <div id="aboutpage" class="outer-box-frame">
 
     <img src="assets/images/vc1_front.png" width="100%" alt="" />
-    <h1>About Projects at the VC</h1>
+    <h1>what is VC?</h1>
+
+<p>
+  Value Collective (VC) is an initiative to support work that is valuable, but not profitable, using alter-economic thinking and practice.
+The goal is to establish support structures for intrinsically-motivated people to develop their projects and ideas away from financial pressures. In the long-term, we envision providing rent-free living arrangements to a rotating cohort of self-organized residents, alongside other forms of support.
+</p>
 
 </div>
+
+<section id=timeline class="outer-box-frame">
+	<h1>timeline</h1>
+	<div class="demo-card-wrapper">
+		<div class="demo-card demo-card--step1">
+			<div class="head">
+				<div class="number-box">
+					<span>01</span>
+				</div>
+				<h2><span class="small">Subtitle</span> Technology</h2>
+			</div>
+			<div class="body">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+				<img src="http://placehold.it/1000x500" alt="Graphic">
+			</div>
+		</div>
+
+		<div class="demo-card demo-card--step2">
+			<div class="head">
+				<div class="number-box">
+					<span>02</span>
+				</div>
+				<h2><span class="small">Subtitle</span> Confidence</h2>
+			</div>
+			<div class="body">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+				<img src="http://placehold.it/1000x500" alt="Graphic">
+			</div>
+		</div>
+
+		<div class="demo-card demo-card--step3">
+			<div class="head">
+				<div class="number-box">
+					<span>03</span>
+				</div>
+				<h2><span class="small">Subtitle</span> Adaptation</h2>
+			</div>
+			<div class="body">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+				<img src="http://placehold.it/1000x500" alt="Graphic">
+			</div>
+		</div>
+
+		<div class="demo-card demo-card--step4">
+			<div class="head">
+				<div class="number-box">
+					<span>04</span>
+				</div>
+				<h2><span class="small">Subtitle</span> Consistency</h2>
+			</div>
+			<div class="body">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+				<img src="http://placehold.it/1000x500" alt="Graphic">
+			</div>
+		</div>
+
+		<div class="demo-card demo-card--step5">
+			<div class="head">
+				<div class="number-box">
+					<span>05</span>
+				</div>
+				<h2><span class="small">Subtitle</span> Conversion</h2>
+			</div>
+			<div class="body">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+				<img src="http://placehold.it/1000x500" alt="Graphic">
+			</div>
+		</div>
+
+	</div>
+</section>
+
+
+
+
+
 
 <link rel="stylesheet" href="css/about.css" />
 
 
+<section class="footerNav">
+  <?php require  "main/footerNav.php" ?>
+</section>
 </section>
 </section>
 <?php require  "main/footer.php" ?>
