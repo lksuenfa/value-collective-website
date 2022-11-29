34c645fa5d6d835e068c4bdbaeeb948bbed72bfb
Atima <atima.ng@hotmail.ca>
2022-11-28 19:41:32 -0500

    Updated events page

--- website/events.php
+++ website/events.php
@@ -17,55 +17,68 @@
     </div>
 
 
-    <div id="documentpage" class="outer-box-frame">
-      <img src="assets/icons/download.svg" alt="download icon">
-      <!-- 435x290px -->
-      <img src="assets/images/thumbnail.png">
+    <div id="eventpage" class="outer-box-frame">
+      <img src="assets/images/projects1.png" alt="teenager group working together">
 
-      <h1>Utilizing our Online Resources</h1>
+      <h1>Event Name Here</h1>
+      <h3>November 1, 2022
+      </br>4th Floor EV</h3>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
+      <h2></h2>
 
     </div>
 
-    <div id="documentpage" class="outer-box-frame">
-      <img src="assets/icons/download.svg" alt="download icon">
+    <div id="eventpage" class="outer-box-frame">
+      <img src="assets/images/projects1.png" alt="teenager group working together">
 
-      <h1>Utilizing our Online Resources</h1>
+      <h1>Event Name Here</h1>
+      <h3>November 1, 2022
+      </br>4th Floor EV</h3>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
+      <h2></h2>
 
     </div>
 
-    <div id="documentpage" class="outer-box-frame">
-      <img src="assets/icons/download.svg" alt="download icon">
+    <div id="eventpage" class="outer-box-frame">
+      <img src="assets/images/projects1.png" alt="teenager group working together">
 
-      <h1>Utilizing our Online Resources</h1>
+      <h1>Event Name Here</h1>
+      <h3>November 1, 2022
+      </br>4th Floor EV</h3>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
+      <h2></h2>
 
     </div>
 
-    <div id="documentpage" class="outer-box-frame">
-      <img src="assets/icons/download.svg" alt="download icon">
+    <div id="eventpage" class="outer-box-frame">
+      <img src="assets/images/projects1.png" alt="teenager group working together">
 
-      <h1>Utilizing our Online Resources</h1>
+      <h1>Event Name Here</h1>
+      <h3>November 1, 2022
+      </br>4th Floor EV</h3>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
+      <h2></h2>
 
     </div>
+    
+    <div id="eventpage" class="outer-box-frame">
+      <img src="assets/images/projects1.png" alt="teenager group working together">
 
-    <div id="documentpage" class="outer-box-frame">
-      <img src="assets/icons/download.svg" alt="download icon">
-
-      <h1>Utilizing our Online Resources</h1>
+      <h1>Event Name Here</h1>
+      <h3>November 1, 2022
+      </br>4th Floor EV</h3>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
+      <h2></h2>
 
     </div>
 
