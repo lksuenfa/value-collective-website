1d9d652bfb7fe8335fb928f1a568e038859f453f
Atima <atima.ng@hotmail.ca>
2022-12-07 11:16:34 -0500

    Added single page php

---
+++ website/single-page.php
@@ -0,0 +1,27 @@
+<?php require "main/header.php" ?>
+
+<section class="body">
+    <?php require "main/nav.php" ?>
+<section>
+        <div id="documentpage2" class="outer-box-frame">
+
+            <h1>Header</h1>
+
+            <div class="page-content">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+                </p>
+            </div>
+
+
+        </div>
+
+
+        <section class="footerNav">
+            <?php require  "main/footerNav.php" ?>
+        </section>
+    </section>
+</section>
+<?php require  "main/footer.php" ?>
